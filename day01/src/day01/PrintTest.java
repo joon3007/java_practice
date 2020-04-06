b4c16b1e109af553284c839ea15ccfdc44ecc85a
joon3007 <joon4141@gmail.com>
2020-04-06 15:47:25 +0900

    print practice

---
+++ day01/src/day01/PrintTest.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class PrintTest {
+	public static void main(String[] args) {
+		//주석 : 해석하고 싶지 않은 줄에 사용하는 기법
+		//1. 소스코드에 설명글을 달 때
+		//2. 기존에 작성되어 있던 소스코드가 당장 필요없을 때 사용한다.
+		/*
+		 * ctrl + shift + / : 주석
+		 * ctrl + shift + \ : 주석 해제
+		 */
+		
+		//인사하는 영역
+		System.out.println("dddd");
+		
+		//소개하는 영역
+		//System.out.println("ddd");
+		
+		
+	}
+}
---
+++ day01/src/day01/PrintTest2.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class PrintTest2 {
+	public static void main(String[] args) {
+		System.out.print("안녕하세요.");
+		System.out.print("안녕하세요.\n");
+		System.out.println("안녕하세요");
+		//ctrl + alt + 방향키 (위,아래):복사
+		//alt + 방향키 : 이동
+		
+		//제어문자
+		// 반드시 따옴표 안에서 작성한다.
+		// \n	: 줄바꿈
+		// \t	: 위아래 간격 맞춰 띄기(tap)
+		// \'	: ' 표현 
+		// \"	: "표현
+		// \\	: 경로작성시 많이 사용됨
+		
+	
+	}
+}
