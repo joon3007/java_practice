ff36a8e887672b4ea8cd5b9899ecdccf7dec0fa7
Administrator <Administrator@E07>
2020-04-09 15:09:13 +0900

    update generic interface

--- day19/src/generic/GnrInter.java
+++ day19/src/generic/GnrInter.java
@@ -1,5 +1,7 @@
 package generic;
 
-public interface GnrInter<T> {
-	public void addDouble();
+public interface GnrInter <N1, N2>{
+	N1 add(N1 data1, N1 data2);
+	N1 sub(N1 data1, N2 data2);
+	N2 mul(N2 data1, N2 data2);
 }
--- day19/src/generic/GnrTest.java
+++ day19/src/generic/GnrTest.java
@@ -10,27 +10,47 @@ class GnrClass<T>{
 	public void setData(T data) {
 		this.data = data;
 	}
-
 }
+
 class GnrMethod{
 	public static <T> T function(T data) {
 		T result = null;
-		if (data instanceof Integer) {
+		if(data instanceof Integer) {
 			result = (T)"정수";
 		}else if(data instanceof Double || data instanceof Float) {
 			result = (T)"실수";
+		}else if(data instanceof Character) {
+			result = (T)"문자";
 		}else if(data instanceof String) {
 			result = (T)"문자열";
 		}
 		return result;
 	}
 }
-public class GnrTest {
+
+public class GnrTest implements GnrInter<Double, Integer>{
 	public static void main(String[] args) {
-		GnrClass<Integer> g = new GnrClass<Integer>();
-		g.setData(10);
-		System.out.println(g.getData());
-		System.out.println(GnrMethod.function(10));
+//		System.out.println(GnrMethod.function(3.14F));
+//		GnrClass<Integer> g = new GnrClass();
+//		g.setData(540);
+//		System.out.println(g.getData());
 	}
-	
+
+	@Override
+	public Double add(Double data1, Double data2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double sub(Double data1, Integer data2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer mul(Integer data1, Integer data2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
